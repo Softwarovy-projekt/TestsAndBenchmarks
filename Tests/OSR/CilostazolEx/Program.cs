f0a0b14048a39890f926e978116911c06c2edf13
Tomas Husak <husaktomas98@gmail.com>
2022-12-12 23:45:52 +0100

    add dummy tests for osr

---
+++ Tests/OSR/CilostazolEx/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int outputA = TestA();
+        int result = TestB(outputA);
+    }
+
+    public static int TestA() {
+        return 1;
+    }
+
+    public static int TestB(int a) {
+        return a + TestA();
+    }
+}
